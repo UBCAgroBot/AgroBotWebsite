137b4ef31dfd6de98ba73218202bed157c916e3c
Arman D <adrismir@gmail.com>
2024-08-09 21:29:00 -0700

    Recruitment timeline improvements & mobile support

--- src/pages/Recruitment.js
+++ src/pages/Recruitment.js
@@ -7,7 +7,7 @@ function SubteamBox({ emoji, title, bulletPoints, bg, overlayFn }) {
     window.scrollTo(0, 0)
   }, [])
 	return (
-		<div className="w-[312px] h-[500px] bg-[#2E1B0F] rounded-[46px] flex flex-col items-center pt-[10px] text-white">
+		<div className="w-[300px] mb-4 lg:mb-0 lg:w-[312px] h-[500px] bg-[#2E1B0F] rounded-[46px] flex flex-col items-center pt-[10px] text-white">
 			<div className="w-[286px] h-[250px] rounded-[46px] flex justify-center items-center" style={{background: bg}}>
 				<p className="text-[128px]">{emoji}</p>
 			</div>
@@ -27,33 +27,33 @@ function Recruitment() {
 	const [showSubteam, setShowSubteam] = useState(false);
 
 	return (
-		<div className="min-h-screen pt-48 pb-32 w-[1000px] mx-auto">
+		<div className="mt-standard pb-32 w-[300px] lg:w-[1000px] mx-auto">
 			{showSubteam ? <SubteamOverlay overlayFn={setShowSubteam} />
 				:
 				null
 			}
-			<h1 className="text-[48px] font-bold">Join the<br />Agrobot team!</h1>
+			<h1 className="text-mobile-header lg:text-header font-bold">Join the<br />Agrobot team!</h1>
 			<Timeline />
-			<h2 className="text-[32px] font-semibold my-12 mt-20">AgroBot Subteams:</h2>
-			<div className="flex justify-between">
+			<h2 className="text-[32px] text-center lg:text-left lg:text-body font-semibold my-12 mt-20">AgroBot Subteams:</h2>
+			<div className="flex flex-col lg:flex-row lg:justify-between">
 				<SubteamBox overlayFn={setShowSubteam} bg="#91D0F2" emoji='🤖' title='Applied AI' bulletPoints={["Object detection", "Plant classification", "Computer vision"]} />
 				<SubteamBox overlayFn={setShowSubteam} bg="#1F5200" emoji='🏎️' title='Chassis' bulletPoints={["Build the agrobot chassis"]} />
 				<SubteamBox overlayFn={setShowSubteam} bg="#91D0F2" emoji='🔋' title='Powertrain' bulletPoints={["Build the agrobot powertrain"]} />
 			</div>
-			<h2 className="text-[32px] font-semibold my-12 mt-20">AgroPonics Subteams:</h2>
-			<div className="flex justify-between">
+			<h2 className="text-[32px] text-center lg:text-left font-semibold my-12 mt-20">AgroPonics Subteams:</h2>
+			<div className="flex flex-col lg:flex-row lg:justify-between">
 				<SubteamBox overlayFn={setShowSubteam} bg="#91D0F2" emoji='🤖' title='Applied AI' bulletPoints={["develop AI models to detect weeds"]} />
 				<SubteamBox overlayFn={setShowSubteam} bg="#1F5200" emoji='🏎️' title='Chassis' bulletPoints={["Build the agrobot chassis"]} />
 				<SubteamBox overlayFn={setShowSubteam} bg="#91D0F2" emoji='🔋' title='Powertrain' bulletPoints={["Build the agrobot powertrain"]} />
 			</div>
-			<h2 className="text-[32px] font-semibold my-12 mt-20">AgroPicker Subteams:</h2>
-			<div className="flex justify-between">
+			<h2 className="text-[32px] text-center lg:text-left font-semibold my-12 mt-20">AgroPicker Subteams:</h2>
+			<div className="flex flex-col lg:flex-row lg:justify-between">
 				<SubteamBox overlayFn={setShowSubteam} bg="#91D0F2" emoji='🤖' title='Applied AI' bulletPoints={["develop AI models to detect weeds"]} />
 				<SubteamBox overlayFn={setShowSubteam} bg="#1F5200" emoji='🏎️' title='Chassis' bulletPoints={["Build the agrobot chassis"]} />
 				<SubteamBox overlayFn={setShowSubteam} bg="#91D0F2" emoji='🔋' title='Powertrain' bulletPoints={["Build the agrobot powertrain"]} />
 			</div>
-			<h2 className="text-[32px] font-semibold my-12 mt-20">Admin Subteams:</h2>
-			<div className="flex justify-between">
+			<h2 className="text-[32px] text-center lg:text-left font-semibold my-12 mt-20">Admin Subteams:</h2>
+			<div className="flex flex-col lg:flex-row lg:justify-between">
 				<SubteamBox overlayFn={setShowSubteam} bg="#91D0F2" emoji='🖍️' title='WebDev' bulletPoints={["suffer in figma and yell at React"]} />
 				<SubteamBox overlayFn={setShowSubteam} bg="#1F5200" emoji='💵' title='Finance' bulletPoints={["Get those sponsorship $$$"]} />
 				<SubteamBox overlayFn={setShowSubteam} bg="#91D0F2" emoji='🎤' title='Marketing' bulletPoints={["Build the agrobot powertrain"]} />
--- src/pages/Sponsorship.js
+++ src/pages/Sponsorship.js
@@ -66,7 +66,7 @@ const Sponsorship = () => {
 			</div>
 			<div className='bg-glass w-full md:w-full overflow-hidden flex after:content[""] after:dark:from-brand-dark after:from-background after:bg-gradient-to-l after:right-0 after:top-0 after:bottom-0 after:w-20 after:z-10 after:absolute before:content[""] before:dark:from-brand-dark before:from-background before:bg-gradient-to-r before:left-0 before:top-0 before:bottom-0 before:w-20 before:z-10 before:absolute'>
 				{[...Array(2)].map((arr, i) => (
-					<div className='flex flex-nowrap animate-slide'>
+					<div key={i}  className='flex flex-nowrap animate-slide'>
 						{items.map((item, i) => (
 							<div key={item.alt} className='relative w-[175px] m-5 shrink-0 flex items-center'>
 								<img src={item.image} alt={item.alt} className="object-contain max-w-none w-[100px] filter" />
