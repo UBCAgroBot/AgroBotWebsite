89fe3e1f68c6b842a79868f8636970d6374bc048
Arman D <adrismir@gmail.com>
2024-08-15 15:49:27 -0700

    Mobile improvements. Fixed width OurMisstion

--- src/components/home/HomeProjects.js
+++ src/components/home/HomeProjects.js
@@ -8,14 +8,12 @@ import AgropickerView from "../projects/agropicker/AgropickerView";
 import { AgrobotModel2D, AgroArm2DModel, AgroponicModel2D } from "../../assets";
 import { useMediaQuery } from "react-responsive";
 
-const AgrobotMod =  <Agrobotmd /> 
-const AgroponicMod =  <AgroponicView /> 
-const AgropickerMod =  <AgropickerView /> 
+const AgrobotMod = <Agrobotmd />
+const AgroponicMod = <AgroponicView />
+const AgropickerMod = <AgropickerView />
 
-function Project({ ProjectName, Text, background, ModelComponent, MobileImg }) {
+function Project({ ProjectName, Text, background, ModelComponent, MobileImg, isMobile }) {
 	const containerRef = useRef(null);
-	// const isMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-	const isMobile = true;
 
 	function useFadeIn(containerRef) {
 		const [isVisible, setIsVisible] = useState(false);
@@ -57,24 +55,28 @@ function Project({ ProjectName, Text, background, ModelComponent, MobileImg }) {
 				<div className="w-full h-full bg-glass rounded-[46px] p-8 shadow-sm">
 					<h2 className="text-mobile-header lg:text-header font-bold mb-4">{ProjectName}</h2>
 					<p className="text-mobile-body">{Text}</p>
-			  <button className="px-4 py-2 mt-4 bg-[#2E1B0F] font-medium text-[24px] rounded-full text-white">Learn More</button>
+					<button className="px-4 py-2 mt-4 bg-[#2E1B0F] font-medium text-[24px] rounded-full text-white">Learn More</button>
 				</div>
 			</animated.div>
 			{!isMobile &&
-			<div className="w-[47.5%] h-[50vh] bg-[#2E1B0F] relative top-[100vh] rounded-lg">
-				{ModelComponent}
-			</div>
+				<div className="w-[47.5%] h-[50vh] bg-[#2E1B0F] relative top-[100vh] rounded-lg">
+					{ModelComponent}
+				</div>
 			}
-			{isMobile && 
-			<div className="w-[47.5%] h-[50vh] bg-[#2E1B0F] relative top-[100vh] rounded-lg">
-				<img src={MobileImg}></img>
-			</div>	
+			{isMobile &&
+				<div className="w-[47.5%] h-[50vh] bg-[#2E1B0F] relative top-[100vh] rounded-lg">
+					<img src={MobileImg}></img>
+				</div>
 			}
 		</div>
 	);
 }
 
 function HomeProjects() {
+
+	// const isMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+	const isMobile = true;
+
 	const projects = [
 		{
 			ProjectName: "AgroBot",
@@ -98,34 +100,26 @@ function HomeProjects() {
 		},
 	]
 
-	return (
-		<div className="bg-[#CDFF70]">
-			{projects.map((proj) => (
-				<Project 
-					ProjectName={proj.ProjectName}
-					Text={proj.Text}
-					ModelComponent={proj.ModelComponent}
-					MobileImg={proj.MobileImg}
-					background={proj.background}
-				/>
-			))}
-			{/* <Project ProjectName="AgroBot" MobileImg={AgrobotModel2D} ModelComponent={AgrobotMod} />
-			<Project
-				ProjectName="AgroPonics "
-				ModelComponent={AgroponicMod}
-				MobileImg={AgroponicModel2D}
-				background={{
-					background: "linear-gradient(to bottom, #CDFF70, #78BE20)",
-				}}
-			/>
-			<Project
-				ProjectName="AgroPicker "
-				ModelComponent={AgropickerMod}
-				background={{ background: "#78BE20" }}
-				MobileImg={AgroArm2DModel}
-			/> */}
-		</div>
-	);
+	if (!isMobile) {
+		return (
+			<div className="bg-[#CDFF70]">
+				{projects.map((proj) => (
+					<Project
+						ProjectName={proj.ProjectName}
+						Text={proj.Text}
+						ModelComponent={proj.ModelComponent}
+						MobileImg={proj.MobileImg}
+						background={proj.background}
+					/>
+				))}
+			</div>
+		);
+	}
+	else {
+		return (
+			<>Mobile Projects</>
+		)
+	}
 }
 
 export default HomeProjects;
