18991855da3a6d458ec90886e65503a2ef9aa267
Arman D <adrismir@gmail.com>
2024-08-19 20:44:46 -0700

    MVP for about section

--- src/components/about/OurStory.jsx
+++ src/components/about/OurStory.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import forest from '../../assets/image/caleb-george.jpg'
 
 function OurStory() {
 
 	return (
-		<div className="w-[300px] pt-32 lg:pt-48 lg:w-full lg:pl-32 lg:min-h-[948px] mx-auto bg-[#CDFF70] text-mobile-body lg:text-body relative">
-			<div className='lg:w-[60%]'>
-				<h3 className="text-mobile-header lg:text-header font-bold pb-8">Our Story</h3>
+		<div className="w-[300px] pt-32 lg:pt-48 lg:w-[1000px] mx-auto bg-[#CDFF70] text-[20px] lg:text-[24px] relative">
+			<div className='lg:w-[100%]'>
+				<h3 className="text-mobile-header lg:text-header font-bold pt-8 pb-4">Our Story</h3>
 				<p className="">
 					Our journey began in 2017 as a skunk works project within another design team, driven by a passion for innovation in agriculture.
 
@@ -15,8 +14,18 @@ function OurStory() {
 					Our journey from a small project to a multi-faceted team has been fueled by our dedication to transforming agriculture for a better, more sustainable future. Join us as we continue to innovate and lead the way in agricultural technology!
 
 				</p>
+				<h3 className="text-mobile-header lg:text-header font-bold pt-8 pb-4">Our Vision</h3>
+				<p className="">
+					We envision an agricultural industry where innovation drives both technology and people, making sustainable practices the standard for all.
+				</p>
+				<h3 className="text-mobile-header lg:text-header font-bold pt-8 pb-4">Our Mission</h3>
+				<p className="">
+					Our mission is to engineer a sustainable future for agriculture through innovation, creativity, and technology. 
+					<br />
+					<br />
+					We aim to cultivate an environment for driven individuals to <strong>plant</strong> seeds of challenge, <strong>grow</strong> ideas into solutions, and <strong>bloom</strong> into action to <strong>harvest</strong> the change we want to see in the world.
+				</p>
 			</div>
-			<div className="hidden lg:block w-[40%] h-[800px] absolute right-0 top-32 bg-cover bg-left-top"></div>
 		</div>
 	)
 }
--- src/components/about/WhoWeAre.jsx
+++ src/components/about/WhoWeAre.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import farm from '../../assets/image/matt-benson.jpg'
 
 function WhoWeAre() {
 
 	return (
-		<div className="w-mobile-content lg:w-full mx-auto my-16 text-mobile-body lg:text-body">
-			<div className="w-[60%]">
-				<h3 className="text-mobile-header lg:text-header font-bold pb-8">What makes a good AgroBotee?</h3>
+		<div className="w-[300px] pt-32 lg:pt-48 lg:w-[1000px] mx-auto bg-[#CDFF70] text-[20px] lg:text-[24px] relative">
+			<div className='lg:w-[100%]'>
+				<h3 className="text-mobile-header lg:text-header font-semibold pb-4">What makes a good AgroBotee?</h3>
 				<p className="mb-6">
 					Being part of UBC Agrobot means embracing innovation, collaboration, curiosity, and integrity, and applying them in everything we do. We are always striving for excellence!
 				</p>
-				<h4 className="font-medium my-2">Here's what makes a good AgroBotee:</h4>
-				<ul className="list-disc ml-12 text-[20px] lg-[text-24px]">
+				<h4 className="font-semibold my-2">Here's what makes a good AgroBotee:</h4>
+				<ul className="list-disc ml-12 text-[20px]">
 					<li className="my-2">Passion for Innovation: Driven by a desire to revolutionize agriculture through cutting-edge technology.</li>
 					<li className="my-2">Team Player: Values teamwork, diverse perspectives, and actively contributes to a supportive, inclusive environment.</li>
 					<li className="my-2">Problem-Solving Skills: Uses strong analytical skills to tackle complex agricultural challenges with innovative solutions.</li>
@@ -20,31 +19,16 @@ function WhoWeAre() {
 					<li className="my-2">Curiosity and Growth Mindset: Eager to learn, explore new concepts, and continuously improve, fostering ongoing innovation.</li>
 					<li className="my-2">Leadership and Initiative: Takes ownership of their work, demonstrates leadership, and inspires others to achieve their best.</li>
 				</ul>
+				<h3 className="text-mobile-header lg:text-header font-semibold pt-8 pb-4">Our Values:</h3>
+				<ol className="list-decimal ml-12 text-[20px]">
+					<li className="my-2"><strong>Innovation:</strong> We are committed to pushing the boundaries of agricultural technology. Our passion for innovation drives us to explore new ideas, embrace cutting-edge solutions, and constantly improve our projects to create a better future for agriculture.</li>
+					<li className="my-2"><strong>Collaboration:</strong> Teamwork is at the heart of everything we do. We believe that diverse perspectives and collective effort lead to the best outcomes. By working together, sharing knowledge, and supporting one another, we achieve greater success.</li>
+					<li className="my-2"><strong>Curiosity:</strong> Our curiosity fuels our desire to learn and grow. We are always eager to explore new concepts, ask questions, and seek out opportunities for improvement. This relentless curiosity helps us stay at the forefront of agricultural advancements.</li>
+					<li className="my-2"><strong>Integrity:</strong> We uphold the highest standards of honesty and ethics in all our actions. Integrity guides our decisions and interactions, ensuring that we build trust within our team and with our partners. We are dedicated to doing what is right, including considering the environmental impact of our work and striving for sustainable solutions.</li>
+				</ol>
 			</div>
 		</div>
 	)
-
-	// return (
-	// 	<div className="bg-[#CDFF70] py-32 flex relative text-mobile-body lg:text-body">
-	// 		<div className='mt-16 ml-[250px] lg:pl-32'>
-	// 			<h3 className="text-mobile-header lg:text-header font-bold pb-8">What makes a good AgroBotee?</h3>
-	// 			<p className="mb-6">
-	// 				Being part of UBC Agrobot means embracing innovation, collaboration, curiosity, and integrity, and applying them in everything we do. We are always striving for excellence!
-	// 			</p>
-	// 			<h4 className="font-medium my-2">Here's what makes a good AgroBotee:</h4>
-	// 			<ul className="list-disc ml-12">
-	// 				<li>Passion for Innovation: Driven by a desire to revolutionize agriculture through cutting-edge technology.</li>
-	// 				<li>Team Player: Values teamwork, diverse perspectives, and actively contributes to a supportive, inclusive environment.</li>
-	// 				<li>Problem-Solving Skills: Uses strong analytical skills to tackle complex agricultural challenges with innovative solutions.</li>
-	// 				<li>Technical Proficiency: Possesses relevant technical knowledge and skills, continuously expanding their expertise.</li>
-	// 				<li>Commitment to Sustainability: Dedicated to developing sustainable solutions that benefit both farmers and the environment.</li>
-	// 				<li>Curiosity and Growth Mindset: Eager to learn, explore new concepts, and continuously improve, fostering ongoing innovation.</li>
-	// 				<li>Leadership and Initiative: Takes ownership of their work, demonstrates leadership, and inspires others to achieve their best.</li>
-	// 			</ul>
-	// 		</div>
-	// 		<div className="hidden lg:block w-[250px] h-[600px] absolute left-0 top-0 mt-32 bg-cover bg-left-top" style={{backgroundImage: `url(${farm})`}}></div>
-	// 	</div>
-	// )
 }
 
 export default WhoWeAre
