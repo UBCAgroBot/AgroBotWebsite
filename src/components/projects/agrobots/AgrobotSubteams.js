3080c9610b8396a92e292b14d1d5e86cee27bf88
Arman D <adrismir@gmail.com>
2024-09-28 13:32:10 -0700

    Ground work for images in projects section

--- src/components/projects/agrobots/AgrobotSubteams.js
+++ src/components/projects/agrobots/AgrobotSubteams.js
@@ -1,4 +1,5 @@
 import ProjectSubteam from "../ProjectSubteam.tsx";
+import AppliedAIBoundingBoxes from "../../../assets/image/AppliedAIBoundingBoxes.png"
 
 function AgrobotSubteams() {
 
@@ -11,6 +12,7 @@ function AgrobotSubteams() {
         title = "Applied AI"
         text="The Applied AI sub-team is dedicated to creating advanced machine learning models that address a variety of AgroBot's tasks. This includes detecting maize and weeds, segmenting grape clusters, and implementing machine vision in the Robot Operating System (ROS). By exploring the intersections between AI research and emerging Agri-Tech, the team continually pushes the boundaries of agricultural innovation. Their work ensures that AgroBot can efficiently and accurately perform its functions in diverse agricultural settings."
         text_pos="left"
+        img = {AppliedAIBoundingBoxes}
       />
       <ProjectSubteam
         title = "Chassis/PowerTrain"
