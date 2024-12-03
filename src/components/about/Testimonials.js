e2adf724a600cd45f1fe8d2d8f0ae8cc974d1134
Sam Shan <yshan06@student.ubc.ca>
2024-12-02 19:56:27 -0800

    fixed formatting a bit

--- src/components/about/Testimonials.js
+++ src/components/about/Testimonials.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 
 function Card({ name, title, quote, image }) {
   return (
-    <li className="bg-glass p-5 rounded-[42px] drop-shadow-2xl shadow-lg w-96">
+    <li className="bg-glass p-5 rounded-[42px] min-w-128">
       <figcaption className="flex">
         <div className="ml-4">
-          <div className="text-[24px] lg:text-[36px] text-[#2E1B0F] font-bold">{name}</div>
-          <div className="text-[16px] lg:text-[24px] text-[#2e1b0fb4] font-bold">{title}</div>
+          <div className="text-[19px] lg:text-[31px] text-[#2E1B0F] font-bold">{name}</div>
+          <div className="text-[11px] lg:text-[19px] text-[#2e1b0fb4] font-bold">{title}</div>
         </div>
       </figcaption>
-      <p className="p-4 text-[16px] lg:text-[24px]">
+      <p className="p-4 text-[11px] lg:text-[19px]">
         {quote}
       </p>
     </li>
@@ -18,7 +18,7 @@ function Card({ name, title, quote, image }) {
 
 function Testimonials() {
 
-  const [showMore, setShowMore] = useState(false)
+  // const [showMore, setShowMore] = useState(false)
 
   const row_one = [
     {
@@ -33,16 +33,6 @@ function Testimonials() {
     { name: "Vishal Thilak", title: "Extermination Team Member", quote: "I like being part of Agrobot because it allows me to work with technology that has real-world applications in agriculture. Having the opportunity to work on the advanced sprayer system for the robot has allowed me to gain interdisciplinary skills, as we’ve had to work with a mix of robotics, software, project management, and even budgeting. Our work here at Agrobot can work to improve current agricultural practices, and knowing that gives me a sense of fulfillment. Plus, the team here is supportive and inclusive of everyone!" },
   ]
 
-  // const row_two = [
-  //   { name: "Cihan Alperen Bosnali", title: "Navigation Team Lead", quote: "Our team's trip to Milan, Italy for the ACRE competition was so memorable! I had the opportunity to meet students, professors, and professionals from all over Europe. It was fascinating to see how different groups came up with wildly different solutions to same problems. Additionally, I had the chance to visit the Artificial Intelligence and Robotics Lab of Politecnico di Milano, where I met with researchers engaged in interesting robotics projects. The technical challenges we had in competition were quite memorable as well! I developed engineering solutions on the go with limited time and resources. It was an exciting, high-adrenaline experience. I learned so much about different quick fixes I can do in all of the mechanical, electrical and software fields." },
-  //   { name: "Fatma Nilgun Abali", title: "Extermination Team Lead", quote: "Currently, we are designing a fully modular agricultural robot extermination mechanism, aiming for 3D printing feasibility. This system leverages AI to detect harmful weeds, significantly reducing herbicide usage and minimizing toxic emissions. A fun fact about me is I grow 20 different varieties of vegetables and fruits in my backyard, and I love turning them into delicious homemade jams!" },
-  // ]
-
-  // const row_three = [
-  //   { name: "Husan Aulakh", title: "Applied AI Lead", quote: "One of my favourite aspects of being on the team is the learning hours at the end of the day. During these sessions, we explore different types of models, learn how to use specific software and gain insights into various aspects of robotics and agriculture. During one of our learning hours, someone presented the Segment Anything Model (SAM) and demonstrated how transfer learning could be used to segment grape clusters. It was quite impressive to see the potential of this technology and how advanced AI models could enhance precision in agricultural applications. It showcased how learning hours could introduce us to innovative solutions and drive our project forward. It was a perfect example of the supportive and educational environment that makes being part of UBC AgroBot so rewarding." },
-  //   { name: "Vishal Thilak", title: "Extermination Team Member", quote: "I like being part of Agrobot because it allows me to work with technology that has real-world applications in agriculture. Having the opportunity to work on the advanced sprayer system for the robot has allowed me to gain interdisciplinary skills, as we’ve had to work with a mix of robotics, software, project management, and even budgeting. Our work here at Agrobot can work to improve current agricultural practices, and knowing that gives me a sense of fulfillment. Plus, the team here is supportive and inclusive of everyone!" },
-  // ]
-
   return (
     <div className="p-4 bg-[#CDFF70] text-[#2E1B0F] relative mt-24">
       <h1 className=" text-[48px] font-bold text-center mt-8 mb-4">
@@ -50,49 +40,19 @@ function Testimonials() {
       </h1>
 
       <div className="p-4 bg-[#CDFF70] text-[#2E1B0F] relative mt-24">
-      <h1 className="text-4xl font-bold text-center mt-8 mb-4">Testimonials</h1>
-      <div className="overflow-x-auto whitespace-nowrap scrollbar-hide text-wrap">
-        <div className="flex space-x-4 px-4">
-          {row_one.map((testimonial, index) => (
-            <Card
-              key={index}
-              name={testimonial.name}
-              title={testimonial.title}
-              quote={testimonial.quote}
-              image={testimonial.image}
-            />
-          ))}
+        <div className="overflow-x-auto whitespace-nowrap scrollbar-hide text-wrap">
+          <div className="flex space-x-4 px-4">
+            {row_one.map((testimonial, index) => (
+              <Card
+                name={testimonial.name}
+                title={testimonial.title}
+                quote={testimonial.quote}
+                // image={testimonial.image}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-      {/* <div className={`grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3 overflow-hidden ${showMore ? '' : 'max-h-[33rem]'} ${showMore ? 'pb-[72px]' : ''}`}>
-				<ul className="space-y-4 my-8">
-					{row_one.map((testimonial, index) => (
-						<Card key={index} name={testimonial.name} title={testimonial.title} quote={testimonial.quote} image={testimonial.image} />
-					))}
-				</ul>
-				<ul className="space-y-4 my-8 hidden sm:block">
-					{row_two.map((testimonial, index) => (
-						<Card key={index} name={testimonial.name} title={testimonial.title} quote={testimonial.quote} image={testimonial.image} />
-					))}
-				</ul>
-				<ul className="space-y-4 my-8 hidden lg:block">
-					{row_three.map((testimonial, index) => (
-						<Card key={index} name={testimonial.name} title={testimonial.title} quote={testimonial.quote} image={testimonial.image} />
-					))}
-				</ul>
-			</div> */}
-      {/* {showMore ?
-        null
-        :
-        <div className="bg-gradient-to-b from-[#CDFF7000] to-[#CDFF70] absolute left-0 bottom-0 w-full h-[22rem]" />
-      }
-      <button
-        onClick={() => setShowMore(!showMore)}
-        className="text-mobile-body lg:text-body bg-glass shadow-lg font-bold px-4 p-2 lg:p-4 lg:px-7 rounded-full absolute bottom-8 left-1/2 transform -translate-x-1/2"
-      >
-        {showMore ? 'Show Less' : 'Show More'}
-      </button> */}
     </div>
   )
 }
